51c44f6a4189caa352fcfea393f4a0fd610bcf75
Julian Giamblanco <giamblanco.julian@gmail.com>
2024-08-16 17:52:46 -0700

    Initial commit

---
+++ lib/ship_compliant_v1_rb.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "ship_compliant_v1_rb/version"
+require_relative "ship_compliant_v1_rb/ship_compliant_v1_rb"
+
+module ShipCompliantV1Rb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ship_compliant_v1_rb_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ShipCompliantV1Rb do
+  it "has a version number" do
+    expect(ShipCompliantV1Rb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
